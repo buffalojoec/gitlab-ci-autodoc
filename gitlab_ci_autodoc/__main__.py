b11409fbb89b3e59ab3e4309c4d022cd89002240
jpcaulfi <joecaulfield29@yahoo.com>
2022-03-19 10:36:24 -0400

    turned into executable pypi lib

--- gitlab_ci_autodoc/__main__.py
+++ gitlab_ci_autodoc/__main__.py
@@ -1,31 +1,19 @@
-import sys, re, os, yaml, mdutils
-from .template_yaml import TemplateYaml
+from gitlab_ci_autodoc import TemplateYaml
+import mdutils, os, re, sys
 
 
 if __name__ == "__main__":
 
-    template_directory = sys.argv[1]
-    print("Commencing auto-doc on directory: {}".format(template_directory))
+    directory = sys.argv[1]
 
     def yaml_only(x): return re.search("(.*.yml|.*.yaml)", x) 
-    included_files = list(filter(yaml_only, os.listdir(template_directory)))
+    yaml_files_detected = list(filter(yaml_only, os.listdir(directory)))
 
-    if len(included_files) != 2: 
-        print("\nError: You must include \"doc.yml\" and one other yaml file in the directory.")
-        print("\nNo other yaml files are allowed.")
-        exit(100)
-    else:
-        for inc_file in included_files:
-            with open(template_directory + inc_file, 'r') as file: 
-                yaml_object = yaml.safe_load(file)
-            if inc_file == "doc.yml":
-                doc_yaml = DocYaml(yaml_object)
-            else:
-                template_yaml = TemplateYaml(yaml_object)
-                
-        mdFile = mdutils.MdUtils(
-            file_name = template_directory + "doc.md",
-            title = template_yaml.name)
-        mdFile.write(doc_yaml.to_markdown())
+    mdFile = mdutils.MdUtils(
+        file_name = sys.argv[1] + "doc.md")
+
+    for yaml_file in yaml_files_detected:
+        template_yaml = TemplateYaml(directory, yaml_file)
         mdFile.write(template_yaml.to_markdown())
-        mdFile.create_md_file()
+
+    mdFile.create_md_file()
